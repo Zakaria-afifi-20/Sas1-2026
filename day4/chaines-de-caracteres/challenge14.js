a7c85eec161b78e5d29e50ab207ce648218af58e
Zakaria-afifi-20 <zakariaafifi17@gmail.com>
2026-09-06 17:00:49 +0100

    update

---
+++ day4/chaines-de-caracteres/challenge14.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")();
+
+function masqueCarte (numero)
+{
+    
+      let num = numero.slice(5)
+      let masqnum = num.padStart(numero.length,"*")
+
+      console.log(masqnum)
+
+}
+let nom = prompt("entre num cart : ")
+masqueCarte(nom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
